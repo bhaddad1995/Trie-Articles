54468719cc5e656d16978f0cf516535dac622e71
Blake Haddad <bhaddad1995@gmail.com>
2018-11-12 21:03:56 -0700

    initial trie strucutre created. Functionality to add strings to trie

---
+++ triesLab.java
@@ -0,0 +1,206 @@
+// Blake Haddad & Andy Nguyen
+// CS-570
+// Tries Assignment
+
+import java.util.Vector;
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+
+class trieNode{
+    public Vector<trieNode> subNodes = new Vector<trieNode>();
+    public char letter;
+
+    trieNode(){
+
+    }
+
+    trieNode(char l){
+        this.letter = l;
+    }
+
+    public Vector<trieNode> getSubNodes(){
+        return this.subNodes;
+    }
+
+    public void setLetter(char letter){
+        this.letter = letter;
+    }
+
+    public char getLetter(){
+        return this.letter;
+    }
+
+    public trieNode addSubNode(trieNode node){
+        int l = subNodes.size();
+        int index = 0;
+        if(l == 0){
+            subNodes.add(node);
+        }else{
+            for(int i=0; i<l; i++){
+                if((int) subNodes.get(i).getLetter() > (int) node.getLetter()){
+                    if(i == 0){
+                        subNodes.insertElementAt(node, 0);
+                        index = 0;
+                        break;
+                    }else if(i == l-1){
+                        subNodes.add(node);
+                        index = l;
+                        break;
+                    }else{
+                        subNodes.insertElementAt(node, i);
+                        index = i;
+                        break;
+                    }
+                }else if(i == l-1){
+                    subNodes.add(node);
+                    index = l;
+                }
+            }
+        }
+        return subNodes.get(index);
+    }
+
+    public trieNode searchSubNodes(char let){
+        int numSubNodes = subNodes.size();
+        for(int i=0; i<numSubNodes; i++){
+            if(subNodes.get(i).getLetter() == let){
+                return subNodes.get(i);
+            }
+            if((int) subNodes.get(i).getLetter() > (int) let){
+                return null;
+            }
+        }
+        return null;
+    }
+
+    public trieNode insertSubLetter(char let){
+        trieNode subLetterNode = searchSubNodes(let);
+        if(subLetterNode == null){
+            trieNode newNode = new trieNode(let);
+            subLetterNode = addSubNode(newNode);
+        }
+        return subLetterNode;
+    }
+
+    public void insertString(String s){
+        int l = s.length();
+        trieNode node = insertSubLetter(s.charAt(0));
+        for(int i=1; i<l; i++){
+            node = node.insertSubLetter(s.charAt(i));
+        }
+    }
+
+    public void printTrieLevels(){
+        int numSubNodes = subNodes.size();
+        if(numSubNodes == 0){
+            System.out.println(getLetter());
+        }else{
+            for(int i = 0; i<numSubNodes; i++){
+                System.out.println(getLetter());
+                trieNode node = subNodes.get(i);
+                node.printTrieLevels();
+            }
+        }
+        return;
+    }
+
+}
+
+class trie{
+
+    public trieNode root = new trieNode();
+
+    public trieNode getRoot(){
+        return this.root;
+    }
+
+    public void print(){
+        root.printTrieLevels();
+    }
+
+    // public void printLeafNodes(treeNode root) 
+    // { 
+    //     // if node is null, return 
+    //     if (root == null){ 
+    //         return;
+    //     }
+        
+    //     // if node is leaf node, print its data     
+    //     if (root.getSubNodes().size() == 0){ 
+    //         System.out.println("Leaf node " + root.getLetter() + " with weight of " + root.getValue() + "and huff code " + huffCode);  
+    //         return; 
+    //     } 
+    
+    //     // if left child exists, check for leaf  
+    //     // recursively 
+    //     if (root.getLeftChild() != null){
+    //         huffCode = huffCode + "0";
+    //         printLeafNodes(root.getLeftChild());
+    //         huffCode = huffCode.substring(0,huffCode.length()-1);
+    //     }
+            
+    //     // if right child exists, check for leaf  
+    //     // recursively 
+    //     if (root.getRightChild() != null){ 
+    //         huffCode = huffCode + "1";
+    //         printLeafNodes(root.getRightChild());
+    //         huffCode = huffCode.substring(0,huffCode.length()-1); 
+    //     }
+    // }  
+
+
+}
+
+class triesLab{
+
+    public String input = "";
+    public static void main(String[] args){
+        triesLab trieObj = new triesLab();
+        trieObj.createTrie();
+    }
+
+
+    public void loadFile(){
+        try {
+            String x;
+            FileReader fileIn = new FileReader("company.dat");
+            BufferedReader in = new BufferedReader(fileIn);
+            x = in.readLine();
+            while (x != null) {
+                input = input + formatInput(x);
+                x = in.readLine();
+            }
+            in.close();
+            fileIn.close();
+
+         } catch (IOException i) {
+            System.err.print("Error loading game with exception:\n"+i);
+            i.printStackTrace();
+            System.exit(0);
+         }
+    }
+
+    public String formatInput(String s){
+        String formattedInput = "";
+        for(int i = 0; i < s.length(); i++){
+            if((int) s.charAt(i) == 9){
+                formattedInput = formattedInput + s.charAt(i);
+            }else{
+                continue;
+            }
+        }
+        return formattedInput;
+    }
+
+    public void createTrie(){
+        trie trieObj = new trie();
+        trieObj.getRoot().insertString("abx");
+        trieObj.getRoot().insertString("abde");
+        trieObj.getRoot().insertString("xyz");
+        trieObj.print();
+        return;
+    }
+
+
+}
